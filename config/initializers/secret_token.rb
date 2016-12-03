e50e6bbe14f90f715440efdf4773773a94b233df
Eddie <eemk117@outlook.com>
2016-12-03 21:33:33 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Roar::Application.config.secret_key_base = 'ca96446fbc2105f1aca563ce95c730c4d4bc19c77115177a69ed85eb70c89fca69b88af62f961bca0bfbadaca47c2a07bc5ce11d8f40b1e22042484e10e5028f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Roar::Application.config.session_store :cookie_store, key: '_roar_session'
