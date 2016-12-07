2ba247b5d630862fa794f1cc83f787f5a7801e3d
Eddie <eemk117@outlook.com>
2016-12-07 22:13:51 +0000

    Connected commens to db

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
   validates :name, length: { minimum: 3 }, presence: true
+  
   validates :address, :description, presence: true
 end
