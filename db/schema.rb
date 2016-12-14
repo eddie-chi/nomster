213d80277aebc464b3d96cfdc249a9bda079d79b
eedie-chi <eemk117@outlook.com>
2016-12-13 19:39:41 -0600

    removed avatar for picture

--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 20161214003821) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "avatar"
+    t.string   "picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
